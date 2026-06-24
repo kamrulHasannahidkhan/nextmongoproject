d92e8d43b5afb53ddc2b80a74254837632481600
kamrulHasannahidkhan <134895166+kamrulHasannahidkhan@users.noreply.github.com>
2026-06-25 01:40:53 +0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navber from "@/components/Navber";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,14 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+      lang="en">
+      <body suppressHydrationWarning >
+        <div className="min-h-full flex flex-col">
+          <Navber />
+          {children}
+        </div>
+
+      </body>
     </html>
   );
 }
---
+++ components/Navber.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Navber() {
+  return (
+    <nav className="flex justify-between items-center px-8 py-3 bg-gray-800 text-white">
+      <Link href="/">Home</Link>
+      <Link href="/addTopic">Add Topic</Link>
+    </nav>
+  );
+}
