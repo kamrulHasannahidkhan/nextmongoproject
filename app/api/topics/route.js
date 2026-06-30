ce85fabd9fc83f602f26fbd5cdfa97a07e1c75d3
kamrulHasannahidkhan <134895166+kamrulHasannahidkhan@users.noreply.github.com>
2026-07-01 03:11:31 +0600

    1july

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -1,1 +1,31 @@
-export async
+import { NextResponse } from "next/server";
+import connectMongoDB  from "@/lib/mongodb";
+import Topic from "@/models/topic";
+
+export async function POST(request) {
+  const { title, description } = await request.json();
+
+  await connectMongoDB();
+  await Topic.create({ title, description });
+
+  return NextResponse.json(
+    { message: "Topic created successfully" },
+    { status: 201 }
+  );
+}
+  
+  export async function GET() {
+    await connectMongoDB();
+    const topics = await Topic.find(); 
+    return NextResponse.json(topics, { status: 200 });
+  }
+
+  export async function DELETE(request) {
+    const id = request.nextUrl.searchParams.get("id");
+    await connectMongoDB();
+    await Topic.findByIdAndDelete(id);
+    return NextResponse.json(
+      { message: "Topic deleted successfully" },
+      { status: 200 }
+    );
+  }
